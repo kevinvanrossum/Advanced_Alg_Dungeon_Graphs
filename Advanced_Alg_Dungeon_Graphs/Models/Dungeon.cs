a7f5a8524f4ace225d8b87c598d83ff5b8f9cf87
Midas Broeders <midasbroeders@hotmail.com>
2018-10-09 12:07:13 +0200

    wip prim

--- Advanced_Alg_Dungeon_Graphs/Models/Dungeon.cs
+++ Advanced_Alg_Dungeon_Graphs/Models/Dungeon.cs
@@ -97,7 +97,23 @@ namespace Advanced_Alg_Dungeon_Graphs.Models
 
         public void ActivateGrenade()
         {
+            IRoom current = StartRoom;
+            int closest = int.MaxValue;
+            IRoom next = null;
+            List<IHallway> options = new List<IHallway>();
+            foreach (var hallway in current.AdjacentHallways) options.Add(hallway);
 
+
+            foreach (var hallway in options)
+            {
+                if (!options.Contains(hallway)) options.Add(hallway);
+                if (hallway.Monster.Level < closest)
+                {
+                    closest = hallway.Monster.Level;
+                    if (hallway.RoomA == current) next = hallway.RoomB;
+                    else if (hallway.RoomB == current) next = hallway.RoomA;
+                }
+            }
         }
 
         public List<IRoom> ActivateCompass()
@@ -161,49 +177,6 @@ namespace Advanced_Alg_Dungeon_Graphs.Models
                 
             }
             return path;
-
-
-            //Dictionary<IRoom, Tuple<IRoom, int>> memory = new Dictionary<IRoom, Tuple<IRoom, int>>();
-            //List<IRoom> previousRooms = new List<IRoom>();
-
-            //memory.Add(StartRoom, new Tuple<IRoom, int>(null, 0));
-
-            //IRoom current = StartRoom;
-
-            //while (!previousRooms.Contains(EndRoom))
-            //{
-            //    foreach (IHallway hallway in current.AdjacentHallways)
-            //    {
-            //        if (hallway.Collapsed) continue;
-            //        int currentDistance = memory[current].Item2;
-
-            //        if (!memory.ContainsKey(hallway.RoomB))memory.Add(hallway.RoomB, new Tuple<IRoom, int>(current, hallway.Monster.Level + currentDistance));
-            //        else if (memory.ContainsKey(hallway.RoomB))
-            //        {
-            //            int futureDistance = currentDistance + hallway.Monster.Level;
-            //            if (futureDistance < memory[hallway.RoomB].Item2) memory[hallway.RoomB] = new Tuple<IRoom, int>(current, futureDistance);
-            //        }
-            //    }
-
-            //    previousRooms.Add(current);
-            //    int max = int.MaxValue;
-            //    foreach (var kvp in memory)
-            //    {
-            //        if (previousRooms.Contains(kvp.Key)) continue;
-            //        if (kvp.Value.Item2 < max)
-            //        {
-            //            max = kvp.Value.Item2;
-            //            current = kvp.Key;
-            //        }
-            //    }
-            //}
-
-            //IRoom reverseTraversal = EndRoom;
-            //while (reverseTraversal != StartRoom)
-            //{
-            //    Console.WriteLine("Je gaat van {0},{1} naar {2},{3}", reverseTraversal.X, reverseTraversal.Y, memory[reverseTraversal].Item1.X, memory[reverseTraversal].Item1.Y);
-            //    reverseTraversal = memory[reverseTraversal].Item1;
-            //}
         }
 
         public string ToPrintable()
